d4385f207c871dffc434e0136c657c03521f4ace
bermylle <bermylle@gmail.com>
2020-05-13 20:28:10 +0800

    add

---
+++ leetcode.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    a = 5
+
+    def climbStairs(n):
+
+        dp = [0]*(n+1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+    print(climbStairs(a))
